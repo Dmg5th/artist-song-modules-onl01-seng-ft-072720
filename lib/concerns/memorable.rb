3cb46fb4c6fd8b3680e991f4dee413415b08ba1b
Daniel Gould <dmgould310@gmail.com>
2020-08-12 21:20:38 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,7 @@
 # require "pry"
 
 module Memorable
-
   module ClassMethods
-
     def reset_all
       self.all.clear
     end
@@ -11,16 +9,13 @@ module Memorable
     def count
       self.all.count
     end
-
   end
-
+  
   module InstanceMethods
-
     def initialize
       self.class.all << self
     end
   end
-
 end
 
 
